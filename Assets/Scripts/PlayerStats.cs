76ca3239b6d723a2f326f2adbcc08ef4c11ba9a6
dotPoozer <dotpoozer@gmail.com>
2014-03-27 20:01:32 +0100

    Instancjonowanie broni z listy w ręce bohatera

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStats : MonoBehaviour {
+
+	public float Strength, Dexterity, Vitality, Charisma, Wisdom;
+	public float Health, Stamina, Faith;
+
+	public Transform LeftHandHold, RightHandHold;
+
+	[System.NonSerialized]
+	public GameObject Weapon;
+
+	[System.NonSerialized]
+	public WeaponsList killingList = new WeaponsList();
+
+	void Start()
+	{
+		killingList.Initialize ();
+		Weapon = (GameObject) Instantiate (Resources.Load (killingList._swords ["shortSword1"].path));
+		Weapon.transform.parent = RightHandHold;
+		Weapon.transform.position = RightHandHold.position + killingList._swords["shortSword1"].offset;
+	}
+
+}
---
+++ Assets/Scripts/WeaponsList.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Klasa przechwująca wszystkie obiekty bedące bronią, klasyfikująca je co do obrażeń, wytrzymałości
+ * wymagań, nazwy, buffów. 
+ **/
+
+public class Sword
+{
+	public string path;
+	public Vector3 offset;
+}
+
+public class WeaponsList : MonoBehaviour
+{
+	public Dictionary<string, Sword> _swords = new Dictionary<string, Sword>();
+
+	public void Initialize()
+	{
+		AddSword("shortSword1");
+	}
+
+	public void AddSword(string path)
+	{
+		Sword temp = new Sword();
+		temp.path = path;
+		temp.offset = new Vector3(0.09f, -0.04f, 0.1f);
+
+		_swords ["shortSword1"] = temp;
+	}
+}
